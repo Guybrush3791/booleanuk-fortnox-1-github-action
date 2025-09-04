16eaf1eeacc4222e69ba91f6b3d57335146d48ba
Guybrush3791 <giovanni@wows.dev>
2025-09-04 09:05:00 +0200

    GitHub Action LC

---
+++ src/main/java/org/booleanuk/demo/BooleanukFortnox1GithubActionApplication.java
@@ -0,0 +1,13 @@
+package org.booleanuk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooleanukFortnox1GithubActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooleanukFortnox1GithubActionApplication.class, args);
+	}
+
+}
