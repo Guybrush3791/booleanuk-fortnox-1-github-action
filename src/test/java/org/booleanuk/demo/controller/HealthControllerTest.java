5d45f044cd4eac68b6ed2171429f75129ff06f32
Guybrush3791 <giovanni@wows.dev>
2025-09-04 09:17:17 +0200

    hotfix

--- src/test/java/org/booleanuk/demo/controller/HealthControllerTest.java
+++ src/test/java/org/booleanuk/demo/controller/HealthControllerTest.java
@@ -1,4 +1,23 @@
 package org.booleanuk.demo.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(HealthControllerTest.class)
 public class HealthControllerTest {
+
+    private HealthController healthController;
+
+    public HealthControllerTest() {
+
+        healthController = new HealthController();
+    }
+
+    @Test
+    public void healthEndpointReturnsOK() {
+
+        assertEquals("OK", healthController.health());
+    }
 }
